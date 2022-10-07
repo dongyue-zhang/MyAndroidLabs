58de37f2aec8241dcbca190eca207766a62e2621
Dongyue Zhang <zhangdongyue22@gmail.com>
2022-10-07 14:01:47 -0400

    Finished week4

--- app/src/main/java/algonquin/cst2335/zhan0758/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/zhan0758/MainActivity.java
@@ -2,24 +2,44 @@ package algonquin.cst2335.zhan0758;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Switch;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imgView;
     Switch sw;
+    Button loginButton;
+    EditText emailEditText, passwordEditText;
+    String emailAddress;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
 
         imgView = findViewById(R.id.flagView);
         sw = findViewById(R.id.spinSwitch);
+        loginButton = findViewById(R.id.loginButton);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        emailAddress = prefs.getString("LoginName", "");
+        emailEditText.setText(emailAddress);
 
         sw.setOnCheckedChangeListener( (btn, isChecked) -> {
             if (isChecked) {
@@ -33,5 +53,48 @@ public class MainActivity extends AppCompatActivity {
                 imgView.clearAnimation();
             }
         });
+
+        Log.w("MainActivity", "In onCreate() - Loading Widgets");
+        loginButton.setOnClickListener(clk -> {
+            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+            SharedPreferences.Editor editor = prefs.edit();
+//            nextPage.putExtra("emailAddress", emailEditText.getText().toString());
+            nextPage.putExtra("password", passwordEditText.getText().toString());
+            editor.putString("LoginName", emailEditText.getText().toString());
+            editor.apply();
+            startActivity(nextPage);
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w("MainActivity", "In onStart() - Visible on screen");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    Log.w("MainActivity", "In onStop() - No longer visible");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w("MainActivity", "In onDestroy() - Any memory used by the application is freed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w("MainActivity", "In onPause() - No longer responds to user input");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("MainActivity", "In Resume() - Responding to user input");
+    }
+
+
 }
---
+++ app/src/main/java/algonquin/cst2335/zhan0758/SecondActivity.java
@@ -0,0 +1,124 @@
+package algonquin.cst2335.zhan0758;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class SecondActivity extends AppCompatActivity {
+    TextView title;
+    Button callNumberButton, changePictureButton;
+    EditText editTextPhone;
+    ImageView profileImage;
+
+    String phoneNumber;
+
+    ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Bitmap thumbnail = data.getParcelableExtra("data");
+//                        profileImage.setImageBitmap(thumbnail);
+
+                        FileOutputStream fOut = null;
+
+                        try {
+                            fOut = openFileOutput("Picture.png", Context.MODE_PRIVATE);
+                            thumbnail.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                            fOut.flush();
+                            fOut.close();
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent fromPrevious = getIntent();
+//        String emailAddress = fromPrevious.getStringExtra("emailAddress");
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        String emailAddress = prefs.getString("LoginName", "");
+        String password = fromPrevious.getStringExtra("password");
+        phoneNumber = prefs.getString("PhoneNumber", "");
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editTextPhone.setText(phoneNumber);
+
+        title = findViewById(R.id.title);
+        title.setText("Welcome back " + emailAddress);
+
+        callNumberButton = findViewById(R.id.callNumberButton);
+        callNumberButton.setOnClickListener(click -> {
+            editTextPhone = findViewById(R.id.editTextPhone);
+
+            phoneNumber = editTextPhone.getText().toString();
+            Intent call = new Intent(Intent.ACTION_DIAL);
+            call.setData(Uri.parse("tel:" + phoneNumber));
+            startActivity(call);
+        });
+
+        changePictureButton = findViewById(R.id.changePictureButton);
+        profileImage = findViewById(R.id.profileImage);
+
+        changePictureButton.setOnClickListener( click -> {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            cameraResult.launch(cameraIntent);
+        });
+
+        File file = new File(getFilesDir(),"Picture.png" );
+        if (file.exists()) {
+            Bitmap theImage = BitmapFactory.decodeFile(getFilesDir() +"/Picture.png");
+            profileImage.setImageBitmap(theImage);
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editor.putString("PhoneNumber", editTextPhone.getText().toString());
+        editor.apply();
+
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("MainActivity", "In Resume() - Responding to user input");
+        File file = new File(getFilesDir(),"Picture.png" );
+        if (file.exists()) {
+            Bitmap theImage = BitmapFactory.decodeFile(getFilesDir() +"/Picture.png");
+            profileImage.setImageBitmap(theImage);
+        }
+    }
+}
