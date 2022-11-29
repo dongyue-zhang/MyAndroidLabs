0e48929c368e1f4713ca79dd89547041c065e6c5
Dongyue Zhang <zhangdongyue22@gmail.com>
2022-11-28 19:19:02 -0500

    Finished week10

--- app/src/main/java/algonquin/cst2335/zhan0758/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/zhan0758/MainActivity.java
@@ -7,11 +7,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -33,11 +29,6 @@ import java.net.URLEncoder;
 
 import algonquin.cst2335.zhan0758.databinding.ActivityMainBinding;
 
-/**
- * Main page of the app that contains a password validation program
- * @author Dongyue Zhang
- * @version 1.0
- */
 public class MainActivity extends AppCompatActivity {
 
     protected String cityName;
@@ -45,12 +36,6 @@ public class MainActivity extends AppCompatActivity {
     Bitmap image;
     String url = null;
     String imgUrl = "https://openweathermap.org/img/w/";
-//    /** Holds the text at the centre of the screen*/
-//    TextView passwordMessage = null;
-//    /** Holds the password input from user*/
-//    EditText passwordFiled = null;
-//    /** Holds the button that checks the input password when clicked*/
-//    Button loginButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -143,93 +128,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-//        setContentView(R.layout.activity_main);
-//        passwordMessage = findViewById(R.id.password_message);
-//        passwordFiled = findViewById(R.id.password_filed);
-//        loginButton = findViewById(R.id.login_button);
-//
-//        loginButton.setOnClickListener( clk -> {
-//            String password = passwordFiled.getText().toString();
-//
-//            if (checkPasswordComplexity( password )) {
-//                passwordMessage.setText("Your password meets the requirements");
-//            } else {
-//                passwordMessage.setText("You shall not pass");
-//            }
-//        });
     }
 
-    /**
-     * Validates given password for at least one uppercase, one lowercase, one number, and one special character.
-     * Gives out a message if it doesn't meet a certain requirement
-     * @param password password that is grabbed from the EditText
-     * @return ture if password meets the requirements; false if not
-     */
-    boolean checkPasswordComplexity(String password) {
-        boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
-        foundUpperCase = false;
-        foundLowerCase = false;
-        foundNumber = false;
-        foundSpecial = false;
-        char[] chars = password.toCharArray();
 
-        for (int i = 0; i < chars.length; i++) {
-            if (Character.isDigit(chars[i])) {
-                foundNumber = true;
-            }
-            if (Character.isUpperCase(chars[i])) {
-                foundUpperCase = true;
-            }
 
-            if (Character.isLowerCase(chars[i])) {
-                foundLowerCase = true;
-            }
-            if (isSpecialCharacter(chars[i])) {
-                foundSpecial = true;
-            }
-        }
 
-        if (!foundUpperCase) {
-            Toast.makeText(this, "Password must contain at lest one uppercase!", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-        else if (!foundLowerCase) {
-            Toast.makeText(this, "Password must contain at lest one lowercase!", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-        else if (!foundNumber) {
-            Toast.makeText(this, "Password must contain at lest one number!", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-        else if (!foundSpecial) {
-            Toast.makeText(this, "Password must contain at lest one special character!", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
-
-    /**
-     * Checks if a given character is a special character
-     * @param c a character
-     * @return true if it's a special character; false if not
-     */
-    boolean isSpecialCharacter(char c) {
-        switch (c) {
-            case '#':
-            case '?':
-            case '*':
-            case '!':
-            case '%':
-            case '^':
-            case '@':
-            case '$':
-            case '&':
-                return true;
-            default:
-                return false;
-        }
-    }
 }
