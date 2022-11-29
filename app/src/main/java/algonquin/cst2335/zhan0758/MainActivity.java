6301505b6a74b9cee96d9d38afc63934ff594ff5
Dongyue Zhang <zhangdongyue22@gmail.com>
2022-11-28 19:15:39 -0500

    Finished week10

--- app/src/main/java/algonquin/cst2335/zhan0758/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/zhan0758/MainActivity.java
@@ -2,11 +2,36 @@ package algonquin.cst2335.zhan0758;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import algonquin.cst2335.zhan0758.databinding.ActivityMainBinding;
 
 /**
  * Main page of the app that contains a password validation program
@@ -14,31 +39,125 @@ import android.widget.Toast;
  * @version 1.0
  */
 public class MainActivity extends AppCompatActivity {
-    /** Holds the text at the centre of the screen*/
-    TextView passwordMessage = null;
-    /** Holds the password input from user*/
-    EditText passwordFiled = null;
-    /** Holds the button that checks the input password when clicked*/
-    Button loginButton = null;
+
+    protected String cityName;
+    protected RequestQueue queue = null;
+    Bitmap image;
+    String url = null;
+    String imgUrl = "https://openweathermap.org/img/w/";
+//    /** Holds the text at the centre of the screen*/
+//    TextView passwordMessage = null;
+//    /** Holds the password input from user*/
+//    EditText passwordFiled = null;
+//    /** Holds the button that checks the input password when clicked*/
+//    Button loginButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        queue = Volley.newRequestQueue(this);
 
-        passwordMessage = findViewById(R.id.password_message);
-        passwordFiled = findViewById(R.id.password_filed);
-        loginButton = findViewById(R.id.login_button);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        loginButton.setOnClickListener( clk -> {
-            String password = passwordFiled.getText().toString();
+        binding.getForecastButton.setOnClickListener(click -> {
+            cityName = binding.cityEditView.getText().toString();
 
-            if (checkPasswordComplexity( password )) {
-                passwordMessage.setText("Your password meets the requirements");
-            } else {
-                passwordMessage.setText("You shall not pass");
+            try {
+                url = "https://api.openweathermap.org/data/2.5/weather?q=" + URLEncoder.encode(cityName, "UTF-8") + "&appid=69b4a40df6f3b050bb7a22edfd16a495&units=metric";
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, (response) -> {
+                    try {
+                        JSONObject coord = response.getJSONObject("coord");
+                        JSONArray weatherArray = response.getJSONArray("weather");
+                        JSONObject position0 = weatherArray.getJSONObject(0);
+                        String description = position0.getString("description");
+                        String iconName = position0.getString("icon");
+                        JSONObject mainObject = response.getJSONObject("main");
+                        double current = mainObject.getDouble("temp");
+                        double min = mainObject.getDouble("temp_min");
+                        double max = mainObject.getDouble("temp_max");
+                        int humidity = mainObject.getInt("humidity");
+
+                        String pathname = getFilesDir() + "/" + iconName + ".png";
+                        File file = new File(pathname);
+                        if (file.exists()) {
+                            image = BitmapFactory.decodeFile(pathname);
+                        } else {
+                            ImageRequest imgReq = new ImageRequest(imgUrl + iconName + ".png", new Response.Listener<Bitmap>() {
+                                @Override
+                                public void onResponse(Bitmap bitmap) {
+                                    image = bitmap;
+                                    FileOutputStream fOut = null;
+                                    try {
+                                        fOut = openFileOutput(iconName + ".png", Context.MODE_PRIVATE);
+                                        image.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                                        fOut.flush();
+                                        fOut.close();
+                                    } catch (FileNotFoundException e) {
+                                        e.printStackTrace();
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                }
+                            }, 1024, 1024, ImageView.ScaleType.CENTER, null, (error) -> {
+
+                            });
+
+                            queue.add(imgReq);
+                        }
+
+                        runOnUiThread(() -> {
+
+                            binding.tempText.setText("The current temperature is " + current);
+                            binding.tempText.setVisibility(View.VISIBLE);
+
+                            binding.minText.setText("The min temperature is " + min);
+                            binding.minText.setVisibility(View.VISIBLE);
+
+                            binding.maxText.setText("The max temperature is " + max);
+                            binding.maxText.setVisibility(View.VISIBLE);
+
+                            binding.humidityText.setText("The humidity is " + humidity + "%");
+                            binding.humidityText.setVisibility(View.VISIBLE);
+
+                            binding.icon.setImageBitmap(image);
+                            binding.icon.setVisibility(View.VISIBLE);
+
+                            binding.descriptionText.setText(description);
+                            binding.descriptionText.setVisibility(View.VISIBLE);
+
+                        });
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }, (error) -> { });
+
+                queue.add(request);
+
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
             }
         });
+
+
+//        setContentView(R.layout.activity_main);
+//        passwordMessage = findViewById(R.id.password_message);
+//        passwordFiled = findViewById(R.id.password_filed);
+//        loginButton = findViewById(R.id.login_button);
+//
+//        loginButton.setOnClickListener( clk -> {
+//            String password = passwordFiled.getText().toString();
+//
+//            if (checkPasswordComplexity( password )) {
+//                passwordMessage.setText("Your password meets the requirements");
+//            } else {
+//                passwordMessage.setText("You shall not pass");
+//            }
+//        });
     }
 
     /**
